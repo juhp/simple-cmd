365cb2e219578c6d65252e654b7ab8d0faf564ab
Jens Petersen <juhpetersen@gmail.com>
2022-03-21 18:56:43 +0800

    initial basic testsuite

---
+++ test/Spec.hs
@@ -0,0 +1,29 @@
+import Test.Hspec
+import SimpleCmd
+
+main :: IO ()
+main = hspec spec
+
+spec :: Spec
+spec = do
+  describe "cmds" $ do
+    it "cmd" $ do
+      out <- cmd "echo" ["hello"]
+      out `shouldBe` "hello"
+
+    it "cmdBool true" $ do
+      ok <- cmdBool "true" []
+      ok `shouldBe` True
+
+    it "cmdBool false" $ do
+      ok <- cmdBool "false" []
+      ok `shouldBe` False
+
+    it "cmdLines" $ do
+      out <- cmdLines "echo" ["1\n2"]
+      out `shouldBe` ["1","2"]
+
+  describe "pipes" $ do
+    it "pipe" $ do
+      out <- pipe ("echo", ["hello"]) ("grep",["hello"])
+      out `shouldBe` "hello"
